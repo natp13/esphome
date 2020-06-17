f52252abe0c14cf66597a972c81aedaf70f6d6d4
natp13 <natp13@gmail.com>
2020-06-17 11:23:51 -0700

    Revert "added energy reading for pzem004 (#1022)"
    This reverts commit 04f1729593d521f82df01cf61797d810f5324c3c.

--- esphome/components/pzem004t/sensor.py
+++ esphome/components/pzem004t/sensor.py
@@ -2,8 +2,7 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor, uart
 from esphome.const import CONF_CURRENT, CONF_ID, CONF_POWER, CONF_VOLTAGE, \
-    CONF_ENERGY, UNIT_VOLT, ICON_FLASH, ICON_COUNTER, UNIT_AMPERE, UNIT_WATT, \
-    UNIT_WATT_HOURS
+    UNIT_VOLT, ICON_FLASH, UNIT_AMPERE, UNIT_WATT
 
 DEPENDENCIES = ['uart']
 
@@ -16,7 +15,6 @@ CONFIG_SCHEMA = cv.Schema({
     cv.Optional(CONF_VOLTAGE): sensor.sensor_schema(UNIT_VOLT, ICON_FLASH, 1),
     cv.Optional(CONF_CURRENT): sensor.sensor_schema(UNIT_AMPERE, ICON_FLASH, 2),
     cv.Optional(CONF_POWER): sensor.sensor_schema(UNIT_WATT, ICON_FLASH, 0),
-    cv.Optional(CONF_ENERGY): sensor.sensor_schema(UNIT_WATT_HOURS, ICON_COUNTER, 0)
 }).extend(cv.polling_component_schema('60s')).extend(uart.UART_DEVICE_SCHEMA)
 
 
@@ -37,7 +35,3 @@ def to_code(config):
         conf = config[CONF_POWER]
         sens = yield sensor.new_sensor(conf)
         cg.add(var.set_power_sensor(sens))
-    if CONF_ENERGY in config:
-        conf = config[CONF_ENERGY]
-        sens = yield sensor.new_sensor(conf)
-        cg.add(var.set_energy_sensor(sens))
